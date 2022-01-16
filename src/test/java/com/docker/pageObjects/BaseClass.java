669e2222bf49104e8a8823774e6b786d55c22e8f
srinivasR-prog1 <sri1.as400@gmail.com>
2022-01-16 21:22:59 +0530

    commit m1

--- src/test/java/com/docker/pageObjects/BaseClass.java
+++ src/test/java/com/docker/pageObjects/BaseClass.java
@@ -1,16 +1,16 @@
 package com.docker.pageObjects;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class BaseClass {
@@ -23,7 +23,7 @@ public class BaseClass {
 	@BeforeTest
 	public void setUp(String browserName) throws MalformedURLException {
 		
-		/*String host = "selenium-hub";
+		String host = "selenium-hub";
 
 		DesiredCapabilities dc = new DesiredCapabilities();
 
@@ -35,9 +35,9 @@ public class BaseClass {
 
 			dc.setBrowserName(BrowserType.FIREFOX);
 
-		}*/
+		}
 		
-		if(browserName.equals("chrome"))
+		/*if(browserName.equals("chrome"))
 		{
 			WebDriverManager.chromedriver().setup();
 		    driver= new ChromeDriver();
@@ -47,10 +47,10 @@ public class BaseClass {
 			WebDriverManager.firefoxdriver().setup();
 			driver= new FirefoxDriver();
 			
-		}
+		}*/
 		
-		/*String completeURL = "http://" + host + ":4444/wd/hub";
-		driver = new RemoteWebDriver(new URL(completeURL), dc);*/
+		String completeURL = "http://" + host + ":4444/wd/hub";
+		driver = new RemoteWebDriver(new URL(completeURL), dc);
 	//	driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
